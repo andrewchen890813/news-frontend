3fdf4ef3d0375fb186ee816a285c3e0e451d5527
andrewchen890813 <k0919042712@gmail.com>
2025-04-12 21:35:36 +0800

    底線

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -24,7 +24,7 @@ const NewsItem = ({ title, content, date, sourceUrl }) => {
   return (
     <div
       className="max-w-4xl mx-auto p-3  border-gray-200 flex flex-col items-start 
-  hover:bg-gray-100  rounded-lg transition duration-200   border-b border-gray-300 pb-4 mb-4"
+  hover:bg-gray-100  rounded-lg transition duration-200 "
     >
       {/* 標題變成連結 */}
       <div className="mb-2">
@@ -60,6 +60,7 @@ const NewsItem = ({ title, content, date, sourceUrl }) => {
           />
         </div>
       )}
+      <div className="absolute bottom-0 left-4 right-4 h-px bg-gray-300" />
     </div>
   );
 };
